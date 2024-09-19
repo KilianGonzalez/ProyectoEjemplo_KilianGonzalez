1ed5facec327884c77f101da4926e8a646e2f051
Kilian <kiliangonzalez@ceroca.cat>
2024-09-19 19:32:07 +0200

    Limites pantalla

--- Assets/Scripts/NuevoJugador.cs
+++ Assets/Scripts/NuevoJugador.cs
@@ -6,10 +6,21 @@ public class NuevoJugador : MonoBehaviour
 {
     private float _vel;
 
+    private Vector2 minPantalla, maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+        minPantalla.x = minPantalla.x + 0.65f;
+        maxPantalla.x = maxPantalla.x - 0.65f;
+
+        minPantalla.y = minPantalla.y + 0.55f;
+        maxPantalla.y = maxPantalla.y - 0.55f;
     }
 
     // Update is called once per frame
@@ -22,7 +33,11 @@ public class NuevoJugador : MonoBehaviour
         Vector2 dir = new Vector2(dirX, dirY);
 
         Vector2 newpos = transform.position;
-        newpos = newpos + dir * _vel * Time.deltaTime; 
+        newpos = newpos + dir * _vel * Time.deltaTime;
+
+        newpos.x = Mathf.Clamp(newpos.x, minPantalla.x, maxPantalla.x);
+        newpos.y = Mathf.Clamp(newpos.y, minPantalla.y, maxPantalla.y);
+
         transform.position = newpos;
     }
 }
